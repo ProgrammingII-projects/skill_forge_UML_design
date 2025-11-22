9c9835781e517fcc0eaa999bcdda4cb620456cb1
Ziad Gebril <zgebro2005@gmail.com>
2025-11-22 22:45:30 +0200

    Admin sevice was fixed

--- src/service/AdminService.java
+++ src/service/AdminService.java
@@ -2,28 +2,40 @@ package service;
 
 import dao.CourseDAO;
 import dao.UserDAO;
+import dao.AdminDAO;
 import model.Course;
 import model.User;
 
 
+
 public class AdminService {
 
-    private final CourseDAO courseDAO;
-    private final AdminDAO adminDAO;
-    private final StudentService studentService;
+    private  CourseDAO courseDAO;
+    private  AdminDAO adminDAO;
+    private  StudentService studentService;
 
-    public AdminService(CourseDAO courseDAO, UserDAO AdminDAO) {
+    public AdminService(CourseDAO courseDAO, AdminDAO AdminDAO) {
         this.courseDAO = courseDAO;
         this.adminDAO = AdminDAO;
     }
 
-    public void approveCourse(Course c) {
-        c.setApproveStatus("approved");
-    }
-
-    public void disapproveCourse(Course c) {
-        c.setApproveStatus("disapproved");
+    public void approveCourse(String courseID) {
+        courseDAO.updateCourseStatus(courseID ,"approved");
     }   
+
+    public void disapproveCourse(String courseID) {
+        courseDAO.updateCourseStatus(courseID ,"disapproved");
+        
+    }   
+
+    public void viewAnalytics(String STudentID) {
+        
+    }   
+
+    
+
+
+    
 
    
 }
